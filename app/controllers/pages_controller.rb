e8511f2292f2949417499763aaf42acee71c28fc
Julia Thuden <julia.thuden@gmail.com>
2023-03-08 20:24:16 +0800

    Fixed buttons on home page and the links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def rules
+  end
+
+  def print
+  end
+
+  def raceinfo
+  end
+
+  def founders
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "pages#home"
+  get "rules", to: "pages#rules"
+  get "print", to: "pages#print"
+  get "raceinfo", to: "pages#raceinfo"
+  get "founders", to: "pages#founders"
 end
